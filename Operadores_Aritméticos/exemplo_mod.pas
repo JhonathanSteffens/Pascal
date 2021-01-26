631803ea7b0271b1979f746aafc479ee91dc1cc8
Jhonathan Christian Steffens <67706272+JhonathanSteffens@users.noreply.github.com>
2021-01-26 11:48:22 -0300

    Update exemplo_mod.pas

--- Operadores_Aritméticos/exemplo_mod.pas
+++ Operadores_Aritméticos/exemplo_mod.pas
@@ -1,8 +1,9 @@
 Program exemplo_mod ;
 var
 	num1, resultado :integer;
+	
 Begin
-  writeln('Digite um número: ');
+  writeln('Digite um nÃºmero: ');
   readln(num1);
   
   resultado := num1 mod 2;
