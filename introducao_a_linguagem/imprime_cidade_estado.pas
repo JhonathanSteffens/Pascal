76961ff34f26f4d518abfb920e2e35b3a380d120
Jhonathan Christian Steffens <67706272+JhonathanSteffens@users.noreply.github.com>
2021-01-26 08:02:18 -0300

    Add files via upload

---
+++ introducao_a_linguagem/imprime_cidade_estado.pas
@@ -0,0 +1,5 @@
+Program imprime_cidade_estado ;
+Begin
+   writeln('minha cidade é Pato Branco!');
+   writeln('meu estado é o Paraná!');
+End.
---
+++ introducao_a_linguagem/imprime_frases.pas
@@ -0,0 +1,8 @@
+Program imprime_frases ;
+Begin
+  writeln('Minha série favorita é Breaking Bad!');
+  writeln('Sorvete de flocos é demais!');
+  writeln('Grêmio é o meu time de coração');
+  writeln('Yes baby, Thank you!');
+  writeln('Just do it!');
+End.
